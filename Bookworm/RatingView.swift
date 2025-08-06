348f6b36cf3d3f37617ab85a55fdb713025da450
Deser7 <deser7@icloud.com>
2025-08-07 01:09:44 +0400

    добавил RatingView

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Наташа Спиридонова on 07.08.2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
