24c1092cb357e8d9e885a7b5e34f989c44ff250a
Deser7 <deser7@icloud.com>
2025-08-06 09:18:22 +0400

    создал исходный проект

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Наташа Спиридонова on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
